84cffb40b721513e73de826e1b7a620213b03878
vigneshvickky123 <vignesh@betelgeusetech.com>
2021-06-03 11:55:10 +0530

    initial commit

--- app/Http/Controllers/sampleController.php
+++ app/Http/Controllers/sampleController.php
@@ -3,28 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Sample;
-use database\migrations\create_samples_table;
 
-class sampleController extends Controller
+class samplecontroller extends Controller
 {
-    public function index() {
+    public function index(){
         return view('sample');
-
     }
 
-    public function store(Request $request) {
 
-        $sample = new Sample;
-        $sample->name = $request->name;
-        $sample->mobile = $request->mobile;
-        $sample->email = $request->email;
-        $sample->save();
-        return redirect('simple');
-        
-
-       
-
-
-    }
 }
